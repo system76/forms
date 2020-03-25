82ca883ff6dd425f8e3d5153e238ea3b2937db48
Blake Kostner <git@btkostner.io>
2020-03-25 16:29:01 -0600

    take screenshots of each button

--- test/browser/sys-form-button/colors.js
+++ test/browser/sys-form-button/colors.js
@@ -5,8 +5,10 @@ import { assertScreenshot } from '../../utility/browser'
 fixture`SysFormButton Colors`
   .page`http://localhost:7684/iframe.html?id=components-button--colors&viewMode=story`
 
-const container = Selector('#root > div > div')
+const button = (i) => Selector(`#root button:nth-child(${i})`)
 
 test('screenshots', async (t) => {
-  await assertScreenshot(t, container, `sys-form-button/colors`)
+  await assertScreenshot(t, button(1), `sys-form-button/colors-normal`)
+  await assertScreenshot(t, button(2), `sys-form-button/colors-primary`)
+  await assertScreenshot(t, button(3), `sys-form-button/colors-secondary`)
 })
