1ec8d7db4d95f3123195ce889543588a78474108
Blake Kostner <git@btkostner.io>
2020-03-27 20:03:34 -0600

    add screenshot assert to sys-form tests

--- test/browser/sys-form/custom-submit.js
+++ test/browser/sys-form/custom-submit.js
@@ -1,8 +1,11 @@
 import { Selector } from 'testcafe'
 
+import { assertScreenshot } from '../../utility/browser'
+
 fixture`SysForm CustomSubmit`
   .page`http://localhost:7684/iframe.html?id=high-level-components-form--custom-submit&viewMode=story`
 
+const form = Selector('form')
 const email = Selector('input#email')
 const password = Selector('input#password')
 const button = Selector('button')
@@ -17,8 +20,10 @@ test('login button is reactive to form errors', async (t) => {
   await t.typeText(password, 'validpassword')
   await t.pressKey('tab')
   await t.expect(button.getAttribute('disabled')).eql(undefined)
+  await assertScreenshot(form, 'sys-form/reactive-enabled')
   await t.typeText(email, 'nope', { replace: true })
   await t.typeText(password, 'nope', { replace: true })
   await t.pressKey('tab')
   await t.expect(button.getAttribute('disabled')).eql('disabled')
+  await assertScreenshot(form, 'sys-form/reactive-disabled')
 })
