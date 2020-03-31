b75265cd23dd2f783922c1123951989df0cdec3a
Blake Kostner <git@btkostner.io>
2020-03-30 23:14:21 -0600

    add extra inputs and form examples

---
+++ src/components/sys-form-password.vue
@@ -0,0 +1,35 @@
+/**
+ * src/components/sys-form-password.vue
+ * A password entry input.
+ */
+
+<script>
+  import { combineRules } from '../utility/validation/utility'
+  import SysFormInput from './sys-form-input.vue'
+
+  export default {
+    name: 'SysFormPassword',
+
+    extends: SysFormInput,
+
+    props: {
+      /** The label text to go above the input */
+      label: {
+        type: String,
+        default: 'Password'
+      },
+
+      /** Placeholder text to put in the input */
+      placeholder: {
+        type: String,
+        default: 'correct battery horse staple'
+      }
+    },
+
+    computed: {
+      inputType () {
+        return 'password'
+      }
+    }
+  }
+</script>
