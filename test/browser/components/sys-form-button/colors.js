c1b035170f21b05dce35bbf6a67f02d13a9b66ab
Blake Kostner <git@btkostner.io>
2020-04-12 00:01:23 -0600

    move browser tests to mirror src folder

---
+++ test/browser/components/sys-form-button/colors.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe'
+
+import { assertScreenshot } from '../../../utility/browser'
+
+fixture`SysFormButton Colors`
+  .page`http://localhost:7684/iframe.html?id=high-level-components-button--colors&viewMode=story`
+
+const button = (i) => Selector(`#root button:nth-child(${i})`)
+
+test('screenshots', async (t) => {
+  await assertScreenshot(button(1), 'components/sys-form-button/colors-normal')
+  await assertScreenshot(button(2), 'components/sys-form-button/colors-primary')
+  await assertScreenshot(button(3), 'components/sys-form-button/colors-secondary')
+})
